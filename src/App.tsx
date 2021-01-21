bb5c4d4fcff1689b0165281f3bf30f2057ee6297
royeradames <royeraadames@gmail.com>
2021-01-21 08:06:33 -0500

    📦add bem html tags and css classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="section">
+        <div className="section__inner">
+          <div className="section__card card">
+            <div className="card__about">
+              {/* about */}
+              <span className="card__about_bold">Victor Crest</span>
+              <span className="about_size_l">26</span>
+              London
+            </div>
+    
+            <div className="card__followers ">
+            	{/* followers */}
+            	<span className="card_bold">80K</span>
+            	Followers
+            </div>
+    
+            <div className="card__likes">
+              {/* likes */}
+              <span className="card_bold">803K</span>
+              Likes            
+            </div>
+
+    
+            <div className="card__photos">
+              {/* photos */}
+              <span className="card_bold">1.4K</span>
+              Photos
+            </div>
+          </div>
+        </div>
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="https://www.linkedin.com/in/royer-adames/" target="_blank" rel="noreferrer">Royer Adames</a>.
+      </div>
+    </section>
     </div>
   );
 }
