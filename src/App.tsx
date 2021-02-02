da608f59bcc6062174629aab36fd36f66ff618c8
royeradames <royeraadames@gmail.com>
2021-02-02 09:39:47 -0500

    📦app looks like given design

--- src/App.tsx
+++ src/App.tsx
@@ -5,56 +5,34 @@ import './css/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <section className="section">
-        <div className="section__inner">
           <div className="card section__card ">
-            <div><img className="card__image" src={"./images/image-victor.jpg"} alt="Vicor Crest front facing "/></div>
+            <div ><img className="card__image" src={"./images/image-victor.jpg"} alt="Vicor Crest front facing "/></div>
             <div className="card__about">
-              {/* about */}
               <p>
-                <span className="card_bold">Victor Crest</span> 26
-              </p>
-              
-            </div>
-            <div className="card__location">
-              <p>
-                London
-              </p>
-
-            </div>
-    
-            <hr className="card_social_media_divider"/>
-            <div className="card__followers ">
-              {/* followers */}
-              <p>
-                <span className="card_bold">80K</span> Followers
+                <span className="card_bold">Victor Crest </span> 
+                <span className="card_age">26</span> 
+                <span className="card_new_line"> London</span>
               </p>
             </div>
     
-            <div className="card__likes">
-              {/* likes */}
-              <p>
+            <div className="card_social_media_divider">
+              <p className="card__followers">
+                <span className="card_bold">80K</span> 
+                <span className="card_new_line">Followers</span>
+              </p>
+    
+              <p className="card__likes">
                 <span className="card_bold">803K</span>
-                Likes
+                <span className="card_new_line">Likes</span>
               </p>            
-            </div>
     
-            <div className="card__photos">
-              {/* photos */}
-              <p>
+              <p className="card__photos">
                 <span className="card_bold">1.4K</span>
-                Photos
+                
+                <span className="card_new_line">Photos</span>
               </p>
             </div>
           </div>
-        </div>
-      <div className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-        Coded by <a href="https://www.linkedin.com/in/royer-adames/" target="_blank" rel="noreferrer">Royer Adames</a>.
-      </div>
-    </section>
-    </div>
   );
 }
 
